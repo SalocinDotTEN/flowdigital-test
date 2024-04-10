2f4f6962a934a8fb7c0b871ae6877a61e326603b
Salocin.TEN <salocin.ten+github@gmail.com>
2024-04-10 21:59:50 +0800

    setup data model

---
+++ app/Http/Controllers/BlogPostsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBlogPostsRequest;
+use App\Http\Requests\UpdateBlogPostsRequest;
+use App\Models\BlogPosts;
+
+class BlogPostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBlogPostsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BlogPosts $blogPosts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BlogPosts $blogPosts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBlogPostsRequest $request, BlogPosts $blogPosts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BlogPosts $blogPosts)
+    {
+        //
+    }
+}
