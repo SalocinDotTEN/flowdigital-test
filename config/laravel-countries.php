c59190099799e463efab229a83820fc80d4faef9
Salocin.TEN <salocin.ten+github@gmail.com>
2024-04-10 10:38:28 +0800

    install countries database and reconfigure the form.

---
+++ config/laravel-countries.php
@@ -0,0 +1,21 @@
+<?php
+
+// config for NerdSnipe/LaravelCountries
+
+return [
+
+    /**
+     * The default country to select if none set
+     */
+    'default_country' => 'MY',
+
+    /**
+     * Where should the component place the JavaScript within your view
+     */
+    'script_stack' => 'scripts',
+
+    /**
+     * Where should the component place the CSS styles within your view
+     */
+    'style_stack' => 'css',
+];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,27 +11,32 @@
 
             <div>
                 <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                    autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                    name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div>
                 <x-label for="gender" value="{{ __('Gender') }}" />
-                <select id="gender" name="gender" required>
+                <select id="gender" class="block mt-1 w-full appearance-none" name="gender"
+                    style="background-color: #111827;" required>
                     <option value="female">Female</option>
                     <option value="male">Male</option>
                 </select>
@@ -44,12 +49,17 @@
 
             <div>
                 <x-label for="mailing_address" value="{{ __('Mailing Address') }}" />
-                <x-input id="mailing_address" class="block mt-1 w-full" type="text" name="mailing_address" required />
+                <x-input id="mailing_address" class="block mt-1 w-full" type="text" name="mailing_address"
+                    required />
             </div>
 
             <div>
                 <x-label for="mailing_country" value="{{ __('Country') }}" />
-                <select name="mailing_addr_country" id="mailing_country">
+                <select name="mailing_addr_country" id="mailing_country" class="block mt-1 w-full appearance-none"
+                    style="background-color: #111827;">
+                    @foreach (LaravelCountries::getCountries()->getData() as $country)
+                        <option value="{{ $country->iso2 }}" {{ $country->iso2 == "MY" ? "selected" : ""}}>{{ $country->name }}</option>
+                    @endforeach
                 </select>
             </div>
 
@@ -61,8 +71,18 @@
 
                             <div class="ms-2">
                                 {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">'.__('Privacy Policy').'</a>',
+                                    'terms_of_service' =>
+                                        '<a target="_blank" href="' .
+                                        route('terms.show') .
+                                        '" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">' .
+                                        __('Terms of Service') .
+                                        '</a>',
+                                    'privacy_policy' =>
+                                        '<a target="_blank" href="' .
+                                        route('policy.show') .
+                                        '" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">' .
+                                        __('Privacy Policy') .
+                                        '</a>',
                                 ]) !!}
                             </div>
                         </div>
@@ -71,7 +91,8 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
