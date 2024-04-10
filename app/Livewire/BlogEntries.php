6035f3a4d50f7e465cdf5e9c5e8347d79f2ea134
Salocin.TEN <salocin.ten+github@gmail.com>
2024-04-10 23:18:15 +0800

    Create crud interface.

---
+++ app/Livewire/BlogEntries.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\BlogPosts;
+
+class BlogEntries extends Component
+{
+    public $blogPosts, $post_date, $title, $meta, $content, $slug, $image, $published;
+    public $isModalOpen = 0;
+    public function render()
+    {
+        $this->blogPosts = BlogPosts::all();
+        return view('livewire.blog-entries');
+    }
+    public function create()
+    {
+        $this->resetCreateForm();
+        $this->openModalPopover();
+    }
+    public function openModalPopover()
+    {
+        $this->isModalOpen = true;
+    }
+    public function closeModalPopover()
+    {
+        $this->isModalOpen = false;
+    }
+    private function resetCreateForm()
+    {
+        $this->post_date = '';
+        $this->title = '';
+        $this->meta = '';
+        $this->content = '';
+        $this->slug = '';
+        $this->image = '';
+        $this->published = '';
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'post_date' => 'required',
+            'title' => 'required',
+            // 'meta' => 'required',
+            'content' => 'required',
+            'slug' => 'required',
+            // 'image' => 'required',
+            'published' => 'required',
+        ]);
+
+        BlogPosts::updateOrCreate([
+            'post_date' => $this->post_date,
+            'title' => $this->title,
+            'meta' => $this->meta,
+            'content' => $this->content,
+            'slug' => $this->slug,
+            'image' => $this->image,
+            'published' => $this->published,
+        ]);
+
+        session()->flash('message', $this->slug ? 'Blog Entry Updated Successfully.' : 'Blog Entry Created Successfully.');
+
+        $this->closeModalPopover();
+        $this->resetCreateForm();
+    }
+
+    public function edit($id)
+    {
+        $blogPosts = BlogPosts::findOrFail($id);
+        // $this->id = $id;
+        $this->post_date = $blogPosts->post_date;
+        $this->title = $blogPosts->title;
+        $this->meta = $blogPosts->meta;
+        $this->content = $blogPosts->content;
+        $this->slug = $blogPosts->slug;
+        $this->image = $blogPosts->image;
+        $this->published = $blogPosts->published;
+
+        $this->openModalPopover();
+    }
+
+    public function delete($id)
+    {
+        BlogPosts::find($id)->delete();
+        session()->flash('message', 'Blog Entry Deleted Successfully.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\BlogEntries;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('blog-entries', BlogEntries::class)->name('blog-entries');
 });
