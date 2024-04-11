fe377e4707cbe063ed680a16d3d353e745e3b3f9
Salocin.TEN <salocin.ten+github@gmail.com>
2024-04-11 22:40:34 +0800

    ensure the layout  is effective.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
     <div class="col-lg-8">
         <!-- Nested row for non-featured blog posts-->
         <div class="row">
-            <div class="col-lg-6">
-                @foreach ($blogPosts as $post)
-                    <!-- Blog post-->
+            @foreach ($blogPosts as $post)
+                <!-- Blog post-->
+                <div class="col-sm">
                     <div class="card mb-4">
                         <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg"
                                 alt="..." /></a>
@@ -17,8 +17,8 @@
                             <a class="btn btn-primary" href="{{ route('article', [$post->slug]) }}">Read more →</a>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
